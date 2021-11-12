c1688d52af595ddfcbf29a45a89f46c2c5592bdd
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-11-12 15:06:45 +0200

    add navigation in utils with links for header footer and aside

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -2,21 +2,20 @@ import React from "react";
 import Link from "../link/link";
 import styles from './footer.module.css';
 import logo from '../../images/blue-origami-bird-flipped.png';
+import getNavigation from "../../utils/navigation";
 
 const Footer = () => {
+    const links = getNavigation();
     return (
         <footer className={styles.container}>
             <ul>
-                <Link href="#" title="Going to 1" type="footer" />
-                <Link href="#" title="Going to 2" type="footer" />
-                <Link href="#" title="Going to 3" type="footer" />
-                <Link href="#" title="Going to 4" type="footer" />
-                <Link href="#" title="Going to 5" type="footer" />
-                <Link href="#" title="Going to 6" type="footer" />
-                <Link href="#" title="Going to 7" type="footer" />
-                <Link href="#" title="Going to 8" type="footer" />
-                <Link href="#" title="Going to 9" type="footer" />
-                <Link href="#" title="Going to 10" type="footer" />
+                {
+                    links.map(navElement => {
+                        return (
+                            <Link href={navElement.link} title={navElement.title} type="footer" />
+                        )
+                    })
+                }
                 <img className={styles.logo} src={logo} alt="" />
             </ul>
             <p className={styles.university}>Software University 2019</p>
--- client/src/components/origami/origami.js
+++ client/src/components/origami/origami.js
@@ -2,11 +2,12 @@ import React from "react";
 import styles from './origami.module.css';
 import image from '../../images/blue-origami-bird.png'
 
-const Origami = ({ description, author }) => {
+const Origami = ({ description, author, index }) => {
     return (
         <div className={styles.container}>
             <img className={styles.image} src={image} alt="" />
             <p className={styles.description}>
+                <span>{index} - </span>
                 {description}
             </p>
             <div className={styles["div container"]}>
