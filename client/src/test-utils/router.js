e84914fa9baab1c9cb4bdb03b71e1fedb39ba5cf
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-11-22 18:41:07 +0200

    add header tests

---
+++ client/src/test-utils/router.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import UserContext from "../Context";
+
+const TestingEnvironment = ({ value, children }) => {
+    return (
+        <BrowserRouter>
+            <Route>
+                <UserContext.Provider value={value}>
+                    {children}
+                </UserContext.Provider>
+            </Route>
+        </BrowserRouter>)
+}
+
+export default TestingEnvironment;
